079971642f5190186855b908d32dbd242b3b85cf
barreiros <david.barreiros@101.es>
2015-12-15 01:33:44 +0100

    He añadido github y gitlab a la creación de repositorios remotos

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-*Arguments list:*
+Dependencies:
+===
+Docker-machine (mac-OSX)
+Docker-compose
+curl
+
+Arguments list:
+===
 
 --e : Set the current environment.
 --f : Set the configuration file path.
---a : Configure Apache server with virtual host.
---l : Create local git repository.
---lr : Create local and remote repository.
---lrc : Create local, remote and clone contents from other repository.
+--g : Create local git repository.
+--gr : Create local and remote repository.
+--grc : Create local, remote and clone contents from other repository.
 --d : Configure Docker environment.
 
 
@@ -56,50 +62,45 @@ Project configuration file settings:
 }
 
 
-*DESARROLLO*
+DESARROLLO
+===
 
-configure: Configurar un nuevo proyecto. 
-====
--l, -lr, -lrc
+*init: Configurar un nuevo proyecto.*
+-g, -gr, -grc
 Crear repositorio en local.
 Asignarle un remote.
 Crear la rama devel.
 Crear el primer commit y subirlo al remoto.
 
--lr, -lrc
+-gr, -grc
 Crear un repositorio remoto.
 
--lrc
-Clonar los contenidos de la rama de desarrollo del proyecto base en la rama devel sin heredar el histórico (puedo clonar el repositorio en una carpeta aparte y trabajar desde ahí).
+-grc
+Clonar los contenidos desde un repositorio. (Tengo que dar la opción de seleccionar la rama que queremos clonar. Si no se indica, será la rama master).
 Eliminar los restos de la clonación.
-
--a
-Crear el virtual host de Apache.
 
 -d
 Comprobar si Docker está activo y si tiene instalado docker-compose.
 Iniciar los contenedores y configurarlos con docker compose.
 Añadir la redirección del dominio a la ip del contenedor en /etc/hosts.
 
-reconfigure: Métodos para volver a configurar algunas partes del proyecto. Por ejemplo: el archivo de configuración, el archivo de wp-config en wordpress...
-====
+*reconfigure: Métodos para volver a configurar algunas partes del proyecto. Por ejemplo: el archivo de configuración, el archivo de wp-config en wordpress...*
 
-pack: Preparar la rama master para el despliegue.
-====
+
+*pack: Preparar la rama master para el despliegue.*
 Compilar los estáticos del proyecto.
 Pasar los archivos necesarios de la rama devel a master.
 Crear un commit en devel con el nombre estandarizado. Por ejemplo, deploy+fecha_actual
 Subirlo al remoto.
 
-start: Iniciar los servicios necesarios.
-====
+*start: Iniciar los servicios necesarios.*
 
 
 
-*PRODUCCIÓN*
+PRODUCCIÓN
+===
 
-deploy: Actualizar los archivos en producción.
-====
+*deploy: Actualizar los archivos en producción.*
 Crear un repositorio, si no existe. El árbol de carpetas del servidor tiene que ser como el de desarrollo, es decir, el repositorio va en el raíz y la parte pública va en la carpeta de la aplicación.
 Conectarme al servidor por ssh.
 Hacer comprobaciones previas en los archivos para evitar problemas, comprobando los permisos de escritura, etc...
@@ -109,4 +110,4 @@ Clonar los contenidos de la rama master en un repositorio temporal.
 Actualizar los archivos del servidor por los nuevos que acado de descargar.
 Borrar el repositorio temporal.
 
-rollback: Volver a la versión anterior de los archivos.
+*rollback: Volver a la versión anterior de los archivos.*
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var color = require('colors');
 var datef = require('dateformat');
 
 var tools, host, events, docker, git;
+var verbose = false;
 var env = 'local';
 var gorillaPath = __dirname;
 var gorillaFolder = '.gorilla';
@@ -17,8 +18,8 @@ var projectPath = process.cwd();
 var templatesPath = gorillaPath + '/templates';
 var composeFile = 'docker-compose.yml';
 
-
 if(argv.e) env = argv.e;
+if(argv.v) verbose = true;
 
 tools = require(__dirname + '/lib/tools.js')(env);
 host = require(__dirname + '/lib/host.js');
@@ -29,60 +30,64 @@ git = require(__dirname + '/lib/git.js');
 events.subscribe('PROMISEME', function(){
     tools.promiseme();
 });
+events.subscribe('VERBOSE', function(systemMessage){
+    if(verbose){
+        console.log(systemMessage);
+    }
+});
+events.subscribe('ERROR', function(error){
+    tools.showError(error);
+});
+events.subscribe('WARNING', function(error){
+    tools.showWarning(error);
+});
+events.subscribe('STEP', function(step){
+    tools.showStep(step);
+});
 
-tools.createGorillaEnvironment(
-    projectPath, 
-    templatesPath,
-    gorillaFile,
-    projectPath + '/' + gorillaFolder
-); // If not exist.
-
-if(argv._[0] === 'configure' || argv._[0] === 'pack' || argv._[0] === 'start'){
+if(argv._[0] === 'init' || argv._[0] === 'pack' || argv._[0] === 'start'){
+    tools.createBaseEnvironment(projectPath, gorillaFile, gorillaFolder);
     eval(argv._[0])();
 }
 
-function configure(){
+function init(){
 
     if (argv.f) tools.setConfigFile(argv.f);
 
-    if (argv.a) tools.promises().push('create-vhost');
-
-    if (argv.d) {
-
-        tools.promises().push(
-            [tools.moveFiles, [templatesPath + '/' + tools.param('docker', 'template'), projectPath + '/' + gorillaFolder]],
-            [tools.setEnvVariables, projectPath + '/' + gorillaFolder + '/**/*'],
-            [docker.check, tools.param('docker', 'machinename')],
-            [docker.start, [tools.param('docker', 'machinename'), projectPath + '/' + gorillaFolder + '/' + composeFile, tools.param('apache', 'vhosturl')]],
-            [host.open, ['http://' + tools.param('apache', 'vhosturl') + ':' + tools.param('apache', 'port'), 7]]
-        );
-    }
-
-    if (argv.l) tools.promises().push(
-            git.initRepo, 
-            [git.createBranch, 'devel'],
-            [git.add, '.'],
-            [git.commit, 'Initial commit'], 
-            [git.push, 'devel']
-        );
-    else if (argv.lr) tools.promises().push(
-            git.createRemote, 
-            git.initRepo, 
-            [git.createBranch, 'devel'],
-            [git.add, '.'],
-            [git.commit, 'Initial commit'], 
-            [git.push, 'devel']
-        );
-    else if (argv.lrc) tools.promises().push(
-            git.createRemote, 
-            git.initRepo, 
-            [git.createBranch, 'devel'], 
+    if (argv.grc) tools.promises().push(
+            [git.createRemote, [tools.param('git', 'platform', ['github', 'bitbucket', 'gitlab']), (tools.param('git', 'platform') !== 'gitlab' ? tools.param('git', 'username') : tools.param('git', 'token')), (tools.param('git', 'platform') !== 'gitlab' ? tools.param('git', 'password') : true), tools.param('git', 'private', ['true', 'false']), tools.param('project', 'slug')]],
+            [git.initRepo, [tools.param('git', 'mainrepo'), projectPath]],
+            [git.createBranch, 'gorilla-devel'], 
             [git.clone, tools.param('git', 'clonefrom')],
             [tools.moveFiles, [__dirname + '/temp_repo/', __dirname + '/', ['.git']]],
             [tools.removeDir, __dirname + '/temp_repo/'],
             [git.add, '.'],
             [git.commit, 'Initial commit'], 
-            [git.push, 'devel']
+            [git.push, 'gorilla-devel']
+        );
+    else if (argv.gr) tools.promises().push(
+            [git.createRemote, [tools.param('git', 'platform', ['github', 'bitbucket', 'gitlab']), (tools.param('git', 'platform') !== 'gitlab' ? tools.param('git', 'username') : tools.param('git', 'token')), (tools.param('git', 'platform') !== 'gitlab' ? tools.param('git', 'password') : true), tools.param('git', 'private', ['true', 'false']), tools.param('project', 'slug')]],
+            [git.initRepo, [tools.param('git', 'mainrepo'), projectPath]],
+            [git.createBranch, 'gorilla-devel'],
+            [git.add, '.'],
+            [git.commit, 'Initial commit'], 
+            [git.push, 'gorilla-devel']
+        );
+    else if (argv.g) tools.promises().push(
+            [git.initRepo, [tools.param('git', 'mainrepo'), projectPath]],
+            [git.createBranch, 'gorilla-devel'],
+            [git.add, '.'],
+            [git.commit, 'Initial commit'], 
+            [git.push, 'gorilla-devel']
+        );
+
+    if (argv.d) tools.promises().push(
+            [tools.createDockerEnvironment, [projectPath, templatesPath, gorillaFile, projectPath + '/' + gorillaFolder]], // If not exist.
+            [tools.moveFiles, [templatesPath + '/' + tools.param('docker', 'template'), projectPath + '/' + gorillaFolder]],
+            [tools.setEnvVariables, projectPath + '/' + gorillaFolder + '/**/*'],
+            [docker.check, tools.param('docker', 'machinename')],
+            [docker.start, [tools.param('docker', 'machinename'), projectPath + '/' + gorillaFolder + '/' + composeFile, tools.param('apache', 'vhosturl')]],
+            [host.open, ['http://' + tools.param('apache', 'vhosturl') + ':' + tools.param('apache', 'port'), 15, 'Waiting for opening your web:']]
         );
 
     if (tools.promises().length) tools.promiseme();
@@ -93,16 +98,16 @@ function pack(){
     if (argv.f) tools.setConfigFile(f);
 
     tools.promises().push(
-        [git.createBranch, 'master'],
+        [git.createBranch, 'gorilla-master'],
         git.reset,
         [git.clone, tools.param('git', 'mainrepo')],
         [tools.moveFiles, [__dirname + '/temp_repo/' + tools.param('project', 'serverfolder'), __dirname + '/', ['.git']]],
         [tools.removeDir, __dirname + '/temp_repo/'],
-        [git.checkout, 'master'], 
+        [git.checkout, 'gorilla-master'], 
         [git.add, '.'],
         [git.commit, 'deploy ' + datef(new Date(), 'yyyy-mm-dd HH:MM:ss')], 
         [git.push, 'master'], 
-        [git.checkout, 'devel']
+        [git.checkout, 'gorilla-devel']
     );
 
     if (tools.promises().length) tools.promiseme();
