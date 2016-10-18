4916e4d949edec5ce26cbc6b428120bfdb12533b
barreiros <david.barreiros@101.es>
2016-10-18 19:17:42 +0200

    Bug en el inicio del container de Apache en Wordpress que iniciaba antes que la base de datos

--- templates/wordpress/apache-checkdb.php
+++ templates/wordpress/apache-checkdb.php
@@ -1,6 +1,40 @@
 <?php
 
-    $mysqli = new mysqli("mysql", "{{database.username}}", "{{database.password}}", '{{database.dbname}}');; 
-    while(!@$mysqli->ping()){}
+    $attempts = 5;
+
+    function checkAttempts(){
+
+        global $attempts;
+
+        if($attempts > 0){
+
+            connect();
+
+        }else{
+
+            exit(0);
+
+        }
+    }
+
+    function connect(){
+
+        global $attempts;
+        
+        $connection = @mysqli_connect("mysql", "{{database.username}}", "{{database.password}}", '{{database.dbname}}');
+        if (mysqli_connect_errno()){
+
+            $attempts -= 1;
+            checkAttempts();
+
+        }else{
+
+            exit(0);
+
+        }
+
+    }
+
+    connect();
 
 ?>
