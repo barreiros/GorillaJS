02c294be72b4072ca648f1cd4670f20861d568fb
barreiros <david.barreiros@101.es>
2016-11-21 19:29:37 +0100

    He quitado el número de intentos limitados en el script que comprueba la conexión a la base de datos porque cuando la base de datos es grande no le da tiempo a iniciarse y falla

--- templates/blank+database/apache-checkdb.php
+++ templates/blank+database/apache-checkdb.php
@@ -1,30 +1,16 @@
 <?php
 
-    $attempts = 5;
-
     function checkAttempts(){
 
-        global $attempts;
+        connect();
 
-        if($attempts > 0){
-
-            connect();
-
-        }else{
-
-            exit(0);
-
-        }
     }
 
     function connect(){
 
-        global $attempts;
-        
         $connection = @mysqli_connect("mysql", "{{database.username}}", "{{database.password}}", '{{database.dbname}}');
         if (mysqli_connect_errno()){
 
-            $attempts -= 1;
             checkAttempts();
 
         }else{
