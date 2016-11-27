6f5874376f8d10d893eee990fc5eabd1a740b295
barreiros <david.barreiros@101.es>
2016-11-27 17:44:08 +0100

    He añadido Express.js a la plantilla de NodeJS y también algunas mejoras

---
+++ templates/nodejs/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+
+    res.send('Another Express.js site built with GorillaJS!');
+
+});
+
+app.listen(80);
+
+console.log('Inicio la app');
+
