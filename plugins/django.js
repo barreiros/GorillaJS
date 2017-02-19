80ef1e28f11c8ed242e097f61bd66b189799e3f1
barreiros <david.barreiros@101.es>
2017-02-19 23:40:45 +0100

    He añadido Redis a las opciones de cache. Todavía falta configurarlo en django

--- plugins/django.js
+++ plugins/django.js
@@ -62,18 +62,17 @@ function modifyComposeFileBefore(gorillaFile, templatePath){
     promisesPack = [
 
         [tools.param, ['database', 'engine', ['SQLite', 'PostgreSQL', 'MySQL']], 'engine'],
-        [configureEngine, [templatePath, '{{engine}}']]
+        [tools.param, ['cache', 'engine', ['No, thanks!', 'Redis', 'Memcached']], 'cache'],
+        [tools.param, ['messages', 'engine', ['yes', 'no']], 'messages'],
+        [configureEngine, [templatePath, '{{engine}}']],
+        [configureCache, [templatePath, '{{cache}}']],
+        [configureMessages, templatePath]
 
     ];
     promises.sandwich(promisesPack);
 
 }
 
-function modifyComposeFileAfter(gorillaFile, templatePath){
-
-    appendEngine(gorillaFile, templatePath);
-
-}
 
 function configureEngine(templatePath, engine){
 
@@ -95,44 +94,157 @@ function configureEngine(templatePath, engine){
 
 }
 
-function appendEngine(gorillaFile, templatePath){
+function configureCache(templatePath, engine){
+
+    if(engine === 'Redis'){
+
+    }else if(engine === 'Memcached'){
+
+        fsx.removeSync(templatePath + '/redis.conf');
+        fsx.removeSync(templatePath + '/docker-compose-redis.yml');
+
+    }else{
+
+        fsx.removeSync(templatePath + '/redis.conf');
+        fsx.removeSync(templatePath + '/docker-compose-redis.yml');
+
+    }
+
+    events.publish('PROMISEME');
+
+}
+
+function configureMessages(templatePath, response){
+
+    if(response === 'no'){
+
+        fsx.removeSync(templatePath + '/docker-compose-rabbitmq.yml');
+
+    }
+
+    events.publish('PROMISEME');
+
+}
+
+function modifyComposeFileAfter(gorillaFile, templatePath){
+
+    var settings, promisesPack;
+
+    settings = JSON.parse(fs.readFileSync(gorillaFile));
+
+    promisesPack = [
+
+        [checkDatabaseEngine, [gorillaFile, templatePath]],
+        [checkCacheEngine, [gorillaFile, templatePath]],
+        [checkMessageEngine, [gorillaFile, templatePath]]
+
+    ];
+    promises.sandwich(promisesPack);
+
+}
+
+function checkDatabaseEngine(gorillaFile, templatePath){
 
     var settings, links, composeFile, engineFile;
 
     settings = JSON.parse(fs.readFileSync(gorillaFile));
     composeFile = templatePath + '/docker-compose.yml';
 
-    if(settings.local.database.engine === 'PostgreSQL'){
+    yaml.load(composeFile, function(fileWeb){
 
-        engineFile = templatePath + '/docker-compose-postgresql.yml';
-        yaml.load(engineFile, function(fileEngine){
+        if(settings.local.database.engine === 'PostgreSQL'){
 
-            yaml.load(composeFile, function(fileWeb){
+            engineFile = templatePath + '/docker-compose-postgresql.yml';
+            yaml.load(engineFile, function(fileEngine){
 
                 fileWeb.services['postgresql'] = fileEngine.services.postgresql;
-                fs.writeFileSync(composeFile, yaml.stringify(fileWeb, 6));
+                fs.writeFileSync(composeFile, yaml.stringify(fileWeb, 6)); 
+
+                events.publish('PROMISEME');
 
             });
 
-        });
+        }else if(settings.local.database.engine === 'MySQL'){
 
-    }else if(settings.local.database.engine === 'MySQL'){
-
-        engineFile = templatePath + '/docker-compose-mysql.yml';
-        yaml.load(engineFile, function(fileEngine){
-
-            yaml.load(composeFile, function(fileWeb){
+            engineFile = templatePath + '/docker-compose-mysql.yml';
+            yaml.load(engineFile, function(fileEngine){
 
                 fileWeb.services['mysql'] = fileEngine.services.mysql;
-                fs.writeFileSync(composeFile, yaml.stringify(fileWeb, 6));
+                fs.writeFileSync(composeFile, yaml.stringify(fileWeb, 6)); 
+
+                events.publish('PROMISEME');
 
             });
 
-        });
+        }else{
 
-    }
+            events.publish('PROMISEME');
 
-    events.publish('PROMISEME');
+        }
+
+    });
+
+}
+
+function checkCacheEngine(gorillaFile, templatePath){
+
+    var settings, links, composeFile, engineFile;
+
+    settings = JSON.parse(fs.readFileSync(gorillaFile));
+    composeFile = templatePath + '/docker-compose.yml';
+
+    yaml.load(composeFile, function(fileWeb){
+
+        if(settings.local.cache.engine === 'Redis'){
+
+            engineFile = templatePath + '/docker-compose-redis.yml';
+            yaml.load(engineFile, function(fileEngine){
+
+                fileWeb.services['redis'] = fileEngine.services.redis;
+                fs.writeFileSync(composeFile, yaml.stringify(fileWeb, 6)); 
+
+                events.publish('PROMISEME');
+
+            });
+
+        }else{
+
+            events.publish('PROMISEME');
+
+        }
+
+    });
+
+}
+
+function checkMessageEngine(gorillaFile, templatePath){
+
+    var settings, links, composeFile, engineFile;
+
+    settings = JSON.parse(fs.readFileSync(gorillaFile));
+    composeFile = templatePath + '/docker-compose.yml';
+
+    yaml.load(composeFile, function(fileWeb){
+
+        if(settings.local.messages.engine === 'yes'){
+
+            engineFile = templatePath + '/docker-compose-rabbitmq.yml';
+            yaml.load(engineFile, function(fileEngine){
+
+                fileWeb.services['rabbitmq'] = fileEngine.services.rabbitmq;
+                fs.writeFileSync(composeFile, yaml.stringify(fileWeb, 6)); 
+
+                events.publish('PROMISEME');
+
+            });
+
+        }else{
+
+            events.publish('PROMISEME');
+
+        }
+
+    });
 
 }
 
