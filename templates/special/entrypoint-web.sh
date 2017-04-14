5dc2375fa56b6d35104c9c681d29982df3270b14
barreiros <david.barreiros@101.es>
2017-04-15 00:05:10 +0200

    Bug que no cargaba el proyecto a través del proxy de la plantilla de pruebas

--- templates/special/entrypoint-web.sh
+++ templates/special/entrypoint-web.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -l
 
-cp /root/templates/apache-httpd.conf /etc/apache2/httpd.conf
-cp /root/templates/index.php /var/www/localhost/htdocs/index.php
+cp /root/templates/apache-httpd.conf /etc/apache2/httpd.conf &&
+cp /root/templates/apache-vhost.conf /etc/apache2/sites-available/{{project.domain}}.conf &&
+
+ln -s /etc/apache2/sites-available/{{project.domain}}.conf /etc/apache2/sites-enabled/{{project.domain}}.conf &&
+
+# apachectl start &&
+#
+# echo 'init' > /var/www/localhost/gorilla_status.txt &&
 
 apachectl start -D FOREGROUND
 
--- templates/special/index.php
+++ templates/special/index.php
@@ -3,9 +3,11 @@
     error_reporting(-1);
     ini_set('display_errors', 'On');
 
-    phpinfo();
+    // phpinfo();
 
-    $mongo_conn = new MongoClient('mongo:27017');
-    $mongo = $mongo_conn->selectDB('store');
+    $mysql = new mysqli('mysql', '{{database.username}}', '{{database.password}}', '{{database.dbname}}'); 
+
+    // $mongo_conn = new MongoClient('mongo:27017');
+    // $mongo = $mongo_conn->selectDB('store');
 
 ?>
