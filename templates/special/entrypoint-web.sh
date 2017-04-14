8756eb52f4ff5802f86b44a37b63958ae8ddfe74
barreiros <david.barreiros@101.es>
2017-04-15 00:22:27 +0200

    He terminado la plantilla básica de Alpine

--- templates/special/entrypoint-web.sh
+++ templates/special/entrypoint-web.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-cp /root/templates/apache-httpd.conf /etc/apache2/httpd.conf &&
 cp /root/templates/apache-vhost.conf /etc/apache2/sites-available/{{project.domain}}.conf &&
 
 ln -s /etc/apache2/sites-available/{{project.domain}}.conf /etc/apache2/sites-enabled/{{project.domain}}.conf &&
