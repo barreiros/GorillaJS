f88de24a6d73c1116b5a8fe7ea8e7171ee468a6f
barreiros <david.barreiros@101.es>
2017-05-28 14:12:08 +0200

    He añadido la carpeta de las bases de datos al backup y he configurado el plugin para que pause los contenedores mientras hace el backup

--- plugins/team.js
+++ plugins/team.js
@@ -11,15 +11,15 @@ var fs = require('fs');
 var fsx = require('fs-extra');
 var path = require('path');
 var yaml = require('yamljs');
-var execSync = require('child_process').execSync;
 var AWS = require('aws-sdk');
 var glob = require('glob');
+var spawn = require('child_process').spawn;
 
-var cross = require(path.join(envPaths.libraries, 'crossExec.js'));
 var variables = require(path.join(envPaths.libraries, 'variables.js'));
 var events = require(path.join(envPaths.libraries, 'pubsub.js'));
 var promises = require(path.join(envPaths.libraries, 'promises.js'));
 var token = require(path.join(envPaths.libraries, 'login.js'));
+var cross = require(path.join(envPaths.libraries, 'crossExec.js'));
 
 events.subscribe('INIT_PLUGINS', init);
 
@@ -36,7 +36,7 @@ function init(gorillaFile){
         promisesPack = [
 
             [token.login],
-            [getPolicy, [gorillaFile, '{{token}}']]
+            [runBackup, [gorillaFile, '{{token}}']]
 
         ];
 
@@ -48,7 +48,7 @@ function init(gorillaFile){
 
 }
 
-function getPolicy(gorillaFile, token){
+function runBackup(gorillaFile, token){
 
     var argTail, data, request, options;
 
@@ -79,7 +79,7 @@ function getPolicy(gorillaFile, token){
 
             // Aquí siempre tengo un error que no inicia esta parte solo la primera vez que ejecuto el comando push. No estoy seguro de si solo pasa si no está creada la carpeta de exportación.
 
-            var serverData, exportPath, dataPath, projectPath, key, directory, command, output, readStream, s3;
+            var serverData, exportPath, key, command, excludes, composePath;
 
             if(!error){
 
@@ -91,43 +91,18 @@ function getPolicy(gorillaFile, token){
 
                     if(!serverData.hasOwnProperty('error')){
 
-                        exportPath = path.join(variables.homeUserPath, variables.proxyName, 'export', data.local.project.id);
+                        // Pauso los contenedores del proyecto para no tener problemas con las bases de datos.
+                        composePath = path.join(variables.projectPath, variables.gorillaFolder, variables.gorillaTemplateFolder, 'docker-compose.yml');
 
-                        // Stop the project to avoid problems with databases.???
+                        cross.exec('docker-compose -f ' + composePath + ' -p ' + data.local.project.domain + ' pause', function(err, stdout, stderr){
 
-
-                        // Comprimo las bases de datos del proyecto.
-                        if(!argv.hasOwnProperty('exclude-data')){ // Si el usuario no ha excluido esta opción.
-
-                            dataPath = path.join(variables.homeUserPath, variables.proxyName, 'data', data.local.project.id);
-                            directory = fs.readdirSync(dataPath);
-
-                            fsx.ensureDirSync(path.join(exportPath, 'data'));
-
-                            for(key in directory){
-
-                                if(fs.lstatSync(path.join(dataPath, directory[key])).isDirectory()){
-
-                                    // Tengo que añadir de alguna manera un archivo con los directorios que quiero excluir.
-
-                                    command = 'docker run -v ' + path.join(exportPath, 'data') + ':/etc/export -v ' + path.join(dataPath, directory[key]) + ':/etc/data busybox /bin/sh -c "cd /etc/data && tar -cf /etc/export/' + directory[key] + '.tar ."';
-                                    // execSync(command);
-
-                                }
-
-                            }
-
-                        }
-
-
-                        // Comprimo los archivos del proyecto.
-                        if(!argv.hasOwnProperty('exclude-files')){ // Si el usuario no ha excluido esta opción.
+                            events.publish('VERBOSE', [err, stderr, stdout]);
 
                             // Recupero el archivo .teamignore y genero un string con los archivos que quiero necesito excluir para pasárselo al comando sync de aws cli.
                             fsx.ensureFileSync(path.join(variables.workingPath, '.teamignore'));
 
-                            // Leo las líneas.
-                            var excludes = fs.readFileSync(path.join(variables.workingPath, '.teamignore')).toString().split('\n').filter(function(line){;
+                            // Convierto el archivo en un listado que pueda usar en el comando.
+                            excludes = fs.readFileSync(path.join(variables.workingPath, '.teamignore')).toString().split('\n').filter(function(line){;
 
                                 if(line !== ''){
 
@@ -140,99 +115,72 @@ function getPolicy(gorillaFile, token){
                                 return '--exclude \'/etc/export/project/' + line + '\'';
 
                             });
-
                             excludes = excludes.join(' ');
 
-                            command = 'docker run -e AWS_DEFAULT_REGION="eu-west-1" -e AWS_ACCESS_KEY_ID="' + serverData.credentials.AccessKeyId + '" -e AWS_SECRET_ACCESS_KEY="' + serverData.credentials.SecretAccessKey + '" -e AWS_SECURITY_TOKEN="' + serverData.credentials.SessionToken + '" -v ' + variables.workingPath + ':/etc/export/project gorillajs/tools /bin/sh -c "duplicity ' + excludes + ' --progress --s3-use-new-style --s3-european-buckets --s3-unencrypted-connection --allow-source-mismatch --no-encryption /etc/export/project s3+http://' + path.join(serverData.bucket, serverData.path, 'project') + '"';
                             
-                            // console.log(command);
+                            // Ejecuto el comando en el contenedor "gorillajs/tools", que es donde tengo instalado Duplicity.
+                            command = spawn('docker', [
+                                    'run',
+                                    '-e',
+                                    'AWS_DEFAULT_REGION=eu-west-1',
+                                    '-e',
+                                    'AWS_ACCESS_KEY_ID=' + serverData.credentials.AccessKeyId,
+                                    '-e',
+                                    'AWS_SECRET_ACCESS_KEY=' + serverData.credentials.SecretAccessKey,
+                                    '-e',
+                                    'AWS_SECURITY_TOKEN=' + serverData.credentials.SessionToken,
+                                    '-v',
+                                    variables.workingPath + ':/etc/export/project',
+                                    '-v',
+                                    path.join(variables.homeUserPath, variables.proxyName, 'data', data.local.project.id) + ':/etc/export/data',
+                                    'gorillajs/tools',
+                                    '/bin/sh',
+                                    '-c',
+                                    'duplicity ' + excludes + ' --progress --s3-use-new-style --s3-european-buckets --s3-unencrypted-connection --allow-source-mismatch --no-encryption /etc/export s3+http://' + path.join(serverData.bucket, serverData.path)
+                            ]);
 
-                            // execSync(command);
+                            command.stdout.on('data', function (data) {
+                                console.log(data.toString());
+                            });
 
-                            console.log(command);
+                            command.stderr.on('data', function (data) {
+                                console.log(data.toString());
+                            });
 
-                            cross.exec(command, function(err, stdout, stderr){
+                            command.on('exit', function (code) {
 
-                                console.log(err, stdout, stderr);
+                                cross.exec('docker-compose -f ' + composePath + ' -p ' + data.local.project.domain + ' unpause', function(err, stdout, stderr){
 
-                                events.publish('VERBOSE', [err, stderr, stdout]);
+                                    events.publish('VERBOSE', [err, stderr, stdout]);
+
+                                });
 
                             });
 
-                        }else{
 
-                            command = 'docker run -v ' + exportPath + ':/etc/export -v ' + variables.workingPath + ':/etc/data busybox /bin/sh -c "cd /etc/data && tar -cf /etc/export/project.tar .gorilla"';
-                            // execSync(command);
-
-                        }
+                        });
 
 
-                        // Comprimo las imágenes del proyecto
-                        if(!argv.hasOwnProperty('exclude-images')){ // Si el usuario no ha excluido esta opción.
-
-                            if(data.local.hasOwnProperty('services')){
-
-                                // Recupero las imágenes que se han modificado del nodo "services" que tengo en el archivo gorillafile.
-                                fsx.ensureDirSync(path.join(exportPath, 'images'));
-
-                                for(key in data.local.services){
-
-                                    command = 'docker save -o ' + path.join(exportPath, 'images', key) + '.tar ' + data.local.services[key];
-                                    // output = execSync(command);
-
-                                }
-
-                            }
-
-                        }
-
-
-                        // Creo las variables globales para identificarme en AWS.
-                        // process.env['AWS_ACCESS_KEY_ID'] = serverData.credentials.AccessKeyId;
-                        // process.env['AWS_SECRET_ACCESS_KEY'] = serverData.credentials.SecretAccessKey;
-                        // process.env['AWS_SESSION_TOKEN'] = serverData.credentials.SessionToken;
-                
-
-                        // Subo el contenido de la carpeta export al bucket.
-                        // glob(path.join(exportPath, '**', '*.{tar,tar.gz}'), function(err, files){
-                        // glob(path.join(variables.workingPath, '**', '*'), function(err, files){
+                        // // Comprimo las imágenes del proyecto
+                        // if(!argv.hasOwnProperty('exclude-images')){ // Si el usuario no ha excluido esta opción.
                         //
-                        //     console.log(files);
+                        //     if(data.local.hasOwnProperty('services')){
                         //
-                        //     s3 = new AWS.S3({
-                        //         params: {
-                        //             apiVersion: '2006-03-01',
-                        //             Bucket: serverData.bucket
-                        //         }
-                        //     });
+                        //         exportPath = path.join(variables.homeUserPath, variables.proxyName, 'export', data.local.project.id);
                         //
-                        //     console.log(serverData);
+                        //         // Recupero las imágenes que se han modificado del nodo "services" que tengo en el archivo gorillafile.
+                        //         fsx.ensureDirSync(path.join(exportPath, 'images'));
                         //
-                        //     for(key in files){
+                        //         for(key in data.local.services){
                         //
-                        //         if(fs.lstatSync(files[key]).isFile()){
-                        //
-                        //             console.log(path.join(serverData.path, files[key].replace(exportPath + '/', '')));
-                        //
-                        //             s3.upload({
-                        //                 Key: path.join(serverData.path, files[key].replace(exportPath + '/', '')),
-                        //                 Body: fs.readFileSync(files[key])
-                        //             }).on('httpUploadProgress', function(e){
-                        //
-                        //                 console.log('Uploaded :: ' + parseInt((evt.loaded * 100) / evt.total) + '%');
-                        //
-                        //             }).send(function(err, data){
-                        //
-                        //                 // console.log(err, data);
-                        //                 console.log('File ' + files[key] + ' uploaded');
-                        //
-                        //             });
+                        //             command = 'docker save -o ' + path.join(exportPath, 'images', key) + '.tar ' + data.local.services[key];
+                        //             // output = execSync(command);
                         //
                         //         }
                         //
                         //     }
                         //
-                        // });
+                        // }
 
                     }else{
 
