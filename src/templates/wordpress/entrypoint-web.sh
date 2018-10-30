cdfae8da643da1956bfa7ddde23aa904b0c1dd99
barreiros <hola@davidbarreiros.es>
2018-10-30 15:08:05 +0100

    Elimino el script que modificaba los permisos en la plantilla de Wordpress

--- src/templates/wordpress/entrypoint-web.sh
+++ src/templates/wordpress/entrypoint-web.sh
@@ -53,8 +53,8 @@ done
 replace_domain || true &&
 
 mkdir -p /var/www/{{project.domain}}/ &&
-chown -R apache:apache /var/www/ &&
-chmod -R g+s /var/www/ &&
+# chown -R apache:apache /var/www/ &&
+# chmod -R g+s /var/www/ &&
 
 # su apache -s /bin/sh -c "unison /var/www/{{project.domain}}_mirror/ /var/www/{{project.domain}}/ -repeat watch -prefer newer -silent" &
 
