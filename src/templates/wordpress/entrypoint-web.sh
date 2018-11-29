d45b19493034062334d6c3b23a985985d3e888aa
barreiros <hola@davidbarreiros.es>
2018-11-30 00:20:46 +0100

    Modifico los permisos de usuario en la plantilla de Wordpress temporalmente

--- src/templates/wordpress/entrypoint-web.sh
+++ src/templates/wordpress/entrypoint-web.sh
@@ -12,6 +12,7 @@ function replace_domain {
 cp /root/templates/apache-vhost.conf /etc/apache2/sites-available/{{project.domain}}.conf &&
 ln -s /etc/apache2/sites-available/{{project.domain}}.conf /etc/apache2/sites-enabled/{{project.domain}}.conf &&
 
+
 apachectl stop && apachectl start &&
 
 echo 'init' > /var/www/{{project.domain}}/application/gorilla-status.txt &&
@@ -52,9 +53,13 @@ done
 
 replace_domain || true &&
 
+
 mkdir -p /var/www/{{project.domain}}/ &&
-# chown -R apache:apache /var/www/ &&
-# chmod -R g+s /var/www/ &&
+chown -R 1000:1000 /var/www/ &&
+chmod -R g+s /var/www/ &&
+
+usermod -u 1000 apache
+groupmod -g 1000 apache 
 
 # su apache -s /bin/sh -c "unison /var/www/{{project.domain}}_mirror/ /var/www/{{project.domain}}/ -repeat watch -prefer newer -silent" &
 
